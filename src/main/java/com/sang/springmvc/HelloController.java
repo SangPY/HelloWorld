3018c989b97891b3c9881fa76ff249d7e5263bfa
dotha <dotha@192.168.2.134>
2022-10-18 09:10:47 +0700

    First commit project

---
+++ src/main/java/com/sang/springmvc/HelloController.java
@@ -0,0 +1,16 @@
+package com.sang.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHello(ModelMap map)
+	{
+		map.addAttribute("msg","Sang Spring MVC");
+		return "hello";
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! SSS</h2>
+</body>
+</html>
